50131e18fd5c65dbe5fc6d222e983c51e940ef7a
urdaneta88 <joseurdaneta16@gmail.com>
2020-02-10 19:26:56 -0400

    Improve the config between dev and production env. -JU

--- webpack.config.js
+++ webpack.config.js
@@ -1,32 +1,59 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer')
+  .BundleAnalyzerPlugin;
 
-module.exports = {
-  entry: './src/index.js',
-  output: {
-    path: path.join(__dirname, '/build'),
-    publicPath: '/',
-    filename: 'bundle.js',
-  },
-  devServer: {
-    contentBase: './build',
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: ['babel-loader', 'eslint-loader'],
+module.exports = (env, argv) => {
+  const isProd = argv.mode === 'production';
+
+  return {
+    entry: './src/index.js',
+    output: {
+      path: path.join(__dirname, '/build'),
+      publicPath: '/',
+      filename: 'bundle.js',
+    },
+    devServer: {
+      contentBase: './build',
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /node_modules/,
+          use: ['babel-loader', 'eslint-loader'],
+        },
+      ],
+    },
+    plugins: isProd
+      ? [
+          new HtmlWebpackPlugin({
+            template: path.resolve('./src/index.html'),
+          }),
+        ]
+      : [
+          new HtmlWebpackPlugin({
+            template: path.resolve('./src/index.html'),
+          }),
+          new BundleAnalyzerPlugin(),
+        ],
+
+    devtool: isProd ? '' : 'inline-source-map',
+
+    performance: {
+      hints: isProd ? false : 'warning',
+    },
+
+    optimization: {
+      splitChunks: {
+        cacheGroups: {
+          commons: {
+            test: /[\\/]node_modules[\\/]/,
+            name: 'vendors',
+            chunks: 'all',
+          },
+        },
       },
-    ],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: path.resolve('./src/index.html'),
-    }),
-  ],
-  devtool: 'inline-source-map',
-  performance: {
-    hints: process.env.NODE_ENV === 'production' ? 'warning' : false,
-  },
+    },
+  };
 };
